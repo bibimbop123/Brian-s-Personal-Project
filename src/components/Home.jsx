1d07b8cc82be28b07e08d459982ea794c742f346
briankim <brianhkim13@gmail.com>
2023-08-09 01:17:13 -0500

    created home and navbar components

---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+export function Home() {
+  //make it return a div with the text "Welcome to Brian's Project"
+  return <div> Welcome to Brian's Project </div>;
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export function Nav() {
+  return (
+    <nav id="navbar">
+      <Link to="/"> Home </Link>
+      {/* <Link to="/about"> About </Link>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/contact">Contact me</Link> */}
+    </nav>
+  );
+}
