942deac05e5a249a5f481a7c00aab4156c5ed669
briankim <brianhkim13@gmail.com>
2023-08-09 22:49:18 -0500

    got components to render inside of home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { About } from "./About";
+import { Contact } from "./Contact";
+import { Portfolio } from "./Portfolio";
 export function Home() {
   //make it return a div with the text "Welcome to Brian's Project"
-  return <div> Welcome to Brian's Project </div>;
+  return (
+    <div>
+      Welcome to Brian's Project
+      <About />
+      <Contact />
+      <Portfolio />
+    </div>
+  );
 }
