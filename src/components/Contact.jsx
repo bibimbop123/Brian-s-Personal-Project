4b75ea59140e6f71f2db8d95ab117693a1989643
briankim <brianhkim13@gmail.com>
2023-08-12 17:38:28 -0500

    finished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,21 +5,35 @@ export function Contact() {
   return (
     <div>
       <div> Contact </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <div>
-       
-          <footer className="footer">
-            
-            <p>Copyright © 2023 Brian Kim</p>
-          </footer>
-        </div>
-      </div>
+      <a
+        href="https://github.com/bibimbop123"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="githubcontact"
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+          alt="Github"
+          width="5px"
+          height="5px"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/brian-kim-0a0a1b1b1/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="linkedincontact"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png"
+          alt="LinkedIn"
+          width="5px"
+          height="5px"
+        />
+      </a>
+      <footer className="footer">
+        <p>Copyright © 2023 Brian Kim</p>
+      </footer>
     </div>
   );
 }
