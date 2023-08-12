59f943cd7fafa559b1c7809a700e887d7c0c04d9
briankim <brianhkim13@gmail.com>
2023-08-11 19:41:03 -0500

    used react-vertical-timeline-component and added icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,18 @@ import { Nav } from "./components/Navbar";
 import { About } from "./components/About";
 import { Portfolio } from "./components/Portfolio";
 import { Contact } from "./components/Contact";
+import WorkIcon from "../assets/WorkIcon.jpg";
+import SchoolIcon from "../assets/SchoolIcon.jpg";
+import { timelineElements } from "./timelineElements";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function App() {
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#f9c74f" };
   return (
     <div className="Application">
       <Nav />
@@ -18,6 +28,45 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+      </div>
+      <div id="timeline">
+        <h1 className="title">Timeline</h1>
+        <VerticalTimeline>
+          {timelineElements.map((element) => {
+            let isWorkIcon = element.icon === "work";
+            let showButton =
+              element.buttonText !== undefined &&
+              element.buttonText !== null &&
+              element.buttonText !== "";
+            return (
+              <VerticalTimelineElement
+                key={element.key}
+                date={element.date}
+                dateClassName="date"
+                iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <h5 className="vertical-timeline-element-subtitle">
+                  {element.location}
+                </h5>
+                <p id="description">{element.description}</p>
+                {showButton && (
+                  <a
+                    className={`button ${
+                      isWorkIcon ? "workButton" : "schoolButton"
+                    }`}
+                    href="/"
+                  >
+                    {element.buttonText}
+                  </a>
+                )}
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import { Portfolio } from "./Portfolio";
 import { Footer } from "./Footer";
 import BrianYouth from "../assets/BrianYouth.jpg";
 import ".././App.css";
+import { ReactComponent as WorkIcon}
 
 export function Home() {
   return (
@@ -26,113 +27,6 @@ export function Home() {
         engineering Jobs!
       </section>
       <br />
-      <br />
-      <br />
-      <div className="timeline-wrapper">
-        <ul className="timeline">
-          <li>
-            <span className="title"> Birth</span>
-            <div className="data">
-              <h3>The day i was born</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-                quos exercitationem recusandae consectetur nam facilis aut atque
-                magnam iusto quas laborum delectus inventore voluptatum. Quam
-                nemo neque ipsa delectus sequi?
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-          <li>
-            <span className="title"> High School</span>
-            <div className="data">
-              <h3>High School</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-                quos exercitationem recusandae consectetur nam facilis aut atque
-                magnam iusto quas laborum delectus inventore voluptatum. Quam
-                nemo neque ipsa delectus sequi?
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-          <li>
-            <span className="title"> College</span>
-            <div className="data">
-              <h3>College</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quas
-                tempora reiciendis explicabo minus neque, laboriosam harum
-                possimus placeat magni eveniet, sapiente, eligendi distinctio
-                animi rerum eaque unde commodi inventore.
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-          <li>
-            <span className="title"> Personal Trainer</span>
-            <div className="data">
-              <h3>Personal Trainer</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quas
-                tempora reiciendis explicabo minus neque, laboriosam harum
-                possimus placeat magni eveniet, sapiente, eligendi distinctio
-                animi rerum eaque unde commodi inventore.
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-          <li>
-            <span className="title"> Restaurant</span>
-            <div className="data">
-              <h3>Restaurant</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quas
-                tempora reiciendis explicabo minus neque, laboriosam harum
-                possimus placeat magni eveniet, sapiente, eligendi distinctio
-                animi rerum eaque unde commodi inventore.
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-          <li>
-            <span className="title"> Laundry</span>
-            <div className="data">
-              <h3>Laundry</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quas
-                tempora reiciendis explicabo minus neque, laboriosam harum
-                possimus placeat magni eveniet, sapiente, eligendi distinctio
-                animi rerum eaque unde commodi inventore.
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-          <li>
-            <span className="title"> Software Engineer</span>
-            <div className="data">
-              <h3>Software Engineer</h3>
-              <small> July 13th, 1996</small>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quas
-                tempora reiciendis explicabo minus neque, laboriosam harum
-                possimus placeat magni eveniet, sapiente, eligendi distinctio
-                animi rerum eaque unde commodi inventore.
-              </p>
-              <span className="close">click to close</span>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <br />
-      <br />
-
       <br />
       <Contact />
       <Portfolio />
