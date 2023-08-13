c23c6cc5be1e02d59dd4734f216746db10e03b94
briankim <brianhkim13@gmail.com>
2023-08-12 19:03:07 -0500

    fixed linkedin link in contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ export function Contact() {
         />
       </a>
       <a
-        href="https://www.linkedin.com/in/brian-kim-0a0a1b1b1/"
+        href="https://www.linkedin.com/in/bibimbop/"
         target="_blank"
         rel="noopener noreferrer"
       >
