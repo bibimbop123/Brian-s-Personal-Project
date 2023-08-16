d21ee0392e5424051ed9a92c3a1b71485c2c8a2e
briankim <brianhkim13@gmail.com>
2023-08-16 02:54:40 -0500

    analytics implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,30 @@ import { Nav } from "./components/Navbar";
 import { About } from "./components/About";
 import { Portfolio } from "./components/Portfolio";
 import { Contact } from "./components/Contact";
+import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics/react";
 
 function App() {
+  inject();
   return (
-    <div className="Application">
-      <Nav />
-      <h1 id="head"> Brian's Project </h1>
-      <div id="main-section">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+    <>
+      <div className="Application">
+        <Nav />
+        <h1 id="head"> Brian's Project </h1>
+        <div id="main-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <br />
+        <br />
+        <br />
       </div>
-      <br />
-      <br />
-      <br />
-    </div>
+      <Analytics />
+    </>
   );
 }
 
